41e802801fcf5fafa1267cdd7cedb8a529bcd4af
Keisuke Takeshima <nep@PCC8017.local>
2016-01-19 20:20:37 +0900

    added `equal` method.
    - `normalize` method. private to public.
    - `isAbsolute` method. public to private.

--- README.md
+++ README.md
@@ -4,8 +4,16 @@
 inspired by [path.js](https://github.com/jinder/path/blob/master/path.js)
 
 
+## Install
+
 ```sh
 $ npm install path-browser
+```
+
+## Usage
+
+```html
+<script src="path.min.js"></script>
 ```
 
 ```js
@@ -13,6 +21,7 @@ var joined = path.join('http://example.com', 'bar/foo', '../css/style.css');
 // return 'http://example.com/bar/css/style.css'
 ```
 
+## Methods
 
 |method|summary|
 |:--|:--|
@@ -26,6 +35,106 @@ var joined = path.join('http://example.com', 'bar/foo', '../css/style.css');
 |`resolve`|複数のパスの相対関係を解決した絶対パスを返す|
 |`relative`|2つの絶対パスを受け取って、2つ目のパスを相対パスで返す|
 
+
+__`parse(pathString)`__  
+
+```js
+path.parse('http://example.com:8080/foo/bar/index.html?value=key#hash');
+//  return {
+//    protocol: 'http:',
+//    domain: 'example.com',
+//    port: '8080',
+//    host: 'example.com:8080',
+//    origin: 'http://example.com:8080',
+//    path: '/foo/bar/index.html'
+//    search: '?key=value',
+//    hash: '#hash',
+//    dir: '/dir/bar',
+//    base: 'index',
+//    ext: '.html'
+//  }
+```
+
+
+__`basename(path [, ext])`__
+
+```js
+path.basename('/foo/bar/index.min.html');
+// return index.min.html
+path.basename('/foo/bar/index.min.html', '.html');
+// return index.min
+```
+
+
+__`dirname(path)`__
+
+```js
+path.dirname('/foo/bar/index.html');
+// return /foo/bar
+```
+
+__`extname(path)`__
+
+```js
+path.extname('/foo/bar/index.html');
+// return .html
+```
+
+
+__`join(url1, url2 [, url3...])`__
+
+```js
+path.join('/foo', 'bar', 'baz/asdf', 'quux', '..');
+// return /foo/bar/baz/asdf
+path.join('http://example.com', 'bar/foo', '../css/style.css');
+// 'http://example.com/bar/css/style.css'
+```
+
+
+__`resolve([from... ,] to)`__
+
+```js
+path.resolve('/foo/bar', './baz');
+// return /foo/bar/baz
+path.resolve('/foo/bar', '../css/style.css');
+// return /dir1/css/style.css
+```
+
+
+__`relative(from, to)`__
+
+```js
+path.relative('/data/orandea/test/aaa', '/data/orandea/impl/bbb');
+// return ../../impl/bbb
+```
+
+
+__`equal([host, ] target)`__
+
+
+```js
+path.equal('/foo/bar/*.html');
+// return true
+path.equal('/foo/bar/index.html', '/foo/**/*.html');
+// return true
+```
+
+if argument value is relative path, and then converted to absolute path. And then calculated from `cwd`.
+
+```js
+path.equal('./index.html');
+// calculated "/current/working/directory/index.html"
+```
+
+
+__`cwd()`__
+
+`return value` is absolute path.
+
+```js
+path.cwd();
+// return [current working directory]
+```
 
 ## License
 
--- dist/path.js
+++ dist/path.js
@@ -1,4 +1,8 @@
-// version 0.0.-1
+/**
+ * version 0.0.-1
+ * @see https://github.com/jinder/path/blob/master/path.js
+ * @see https://nodejs.org/api/path.html#path_path_relative_from_to
+ */
 
 var Name = 'path';
 ;(function (root, factory) {
@@ -16,35 +20,37 @@ var Name = 'path';
   var root = window || this;
   var doc = root.document;
   var loc = root.location;
-  var a = document.createElement('a');
+
+  // prototype method shortcut
+  var slice = [].slice;
 
   // regExp
-  var reURL = /^(?:([A-za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/
-  var reFilename = /([^/]*)\.([^/]*)$/;
-  var reExtname = /\.([^/.]+)$/;
-  var reProtocol = /(^http(s)?\:\/\/)/;
-  var reSeparator = /\//;
-  var reDirname = /(?:([^.]*)\/)*/;
+  //          | protocol             | host        | port       | path     | search      | hash
+  var reURL = /^([\w]+:)?(?:(?:\/{2,})([\w\d.\-]+))?(?::([\d]+))?(\/[^?#]*)?(\?(?:[^#]*))?(#(?:.*))?$/;
+
+  var reBase = /([^/?#]*)$/;
+  var reDir = /(?:([^?#]*)\/)*/;
+  var reExt = /(?:[^./]+)(\.[^/.]+)$/;
+  var rePro = /(?:^[\w]+?\:\/{2,})/;
+  var reSep = /\//;
 
 
   /**
    * Path.js API
    * @return {Object}
-   * @see https://github.com/jinder/path/blob/master/path.js
-   * @see https://nodejs.org/api/path.html#path_path_relative_from_to
    */
-
   return (function() {
     return {
       parse     : parse,
       basename  : basename,
       dirname   : dirname,
       extname   : extname,
-      isAbsolute: isAbsolute,
       join      : join,
       cwd       : cwd,
       resolve   : resolve,
-      relative  : relative
+      relative  : relative,
+      normalize : normalize,
+      equal     : equal
     };
   })();
 
@@ -55,30 +61,33 @@ var Name = 'path';
    * @return {Object}
    */
   function parse(pathString) {
+    var a = document.createElement('a');
     a.href = pathString;
-    var parsed = splitPath(a.href);
-    var protocol = parsed[1] + ':';
-    var domain = parsed[3];
-    var port = parsed[4];
-    var path = '/' + parsed[5];
-    var search = '?' + parsed[6];
-    var hash = '#' + parsed[7];
-    var base = path.match(reFilename);
-    var ext = path.match(reExtname);
-    var dir = path.match(reDirname);
+    var allParts = splitPath(a.href);
+    var protocol = allParts[0];
+    var domain = allParts[1];
+    var port = allParts[2];
+    var path = allParts[3];
+    var search = allParts[4];
+    var hash = allParts[5];
+    var host = domain + (port ? ':' + port :'');
+    var origin = protocol + '//' + host;
+    var dir = reDir.test(path) ? reDir.exec(path)[1] : '';
+    var base = reBase.test(path) ? reBase.exec(path)[1] : '';
+    var ext = reExt.test(path) ? reExt.exec(path)[1] : '';
 
     return {
       protocol  : protocol,
       domain    : domain,
       port      : port,
-      host      : domain + ':' + port,
-      origin    : protocol + '//' + domain + ':' + port,
+      host      : host,
+      origin    : origin,
       path      : path,
       search    : search,
       hash      : hash,
-      dir       : dir && dir[1],
-      base      : base && base[1],
-      ext       : ext && ('.' + ext[1])
+      dir       : dir,
+      base      : base,
+      ext       : ext
     };
   }
 
@@ -106,23 +115,24 @@ var Name = 'path';
 
 
   /**
-   * get the file name by [pathname]
+   * get the file name from [pathname]
    * @param {String} p <pathname>
    * @param {String} ext <.extname>
+   * @return {String} <file name>
    */
   function basename(p, ext) {
-    var matched = p.match(reFilename);
-    if (!matched) return '';
-    if (ext && ext.replace(/^\./,'') === matched[2]) {
-      return matched[1];
+    var f = parse(p).base;
+    if (ext && f.substr(-1 * ext.length) === ext) {
+      f = f.substr(0, f.length - ext.length);
     }
-    return matched[0];
+    return f;
   }
 
 
   /**
-   * get the directory name by [pathname]
+   * get the directory name from [pathname]
    * @param {String} p <pathname>
+   * @return {String} <directory name>
    */
   function dirname(p) {
     var parsed = parse(p).path;
@@ -133,47 +143,22 @@ var Name = 'path';
 
 
   /**
-   * get the extension name by [pathname]
-   * @param {String} p <pathname >
-   * @return {String} extname <.ext> (added dot)
+   * get the extension name from [pathname]
+   * @param {String} p <pathname>
+   * @return {String} extname <.ext> (contains dot)
    */
   function extname(p) {
-    var parsed = parse(p).path;
-    var filename = basename(parsed);
-    if (!filename || filename.charAt(0) === '.') return '';
-    var matched = filename.match(reExtname);
-    if (!matched) return '';
-    return matched[0];
-  }
-
-
-  /**
-   * is absolute path ?
-   * @param {String} p <pathname>
-   * @return {Boolean}
-   */
-  function isAbsolute(p) {
-    return p.charAt(0) === '/';
-  }
-
-
-  /**
-   * is domain ?
-   * @param {String} p <pathname>
-   * @return {Boolean}
-   */
-  function isDomain(p) {
-    return reProtocol.test(p);
+    return parse(p).ext;
   }
 
 
   /**
    * join the pathes
-   * @param {String} [url1, url2, url3...]
-   * @return {String}
+   * @param {String} [path1, path2, path3...]
+   * @return {String} joined path
    */
   function join() {
-    var args = Array.prototype.slice.call(arguments, 0);
+    var args = slice.call(arguments, 0);
     var path = '';
 
     for (var i = 0; i < args.length; i += 1) {
@@ -191,6 +176,119 @@ var Name = 'path';
     }
 
     return normalize(path);
+  }
+
+
+  /**
+   * resolve
+   * @param {String} [from ...]
+   * @param {String} to
+   */
+  function resolve() {
+    var resolvedPath = '';
+    var resolvedAbsolute = false;
+    var hasDomain = false;
+
+    for (var i = arguments.length - 1; i >= -1 && !resolvedAbsolute; i -= 1) {
+      var p = (i >= 0) ? arguments[i] : !hasDomain ? cwd() : '';
+
+      if (!hasDomain) {
+        hasDomain = isDomain(p);
+      }
+
+      if (!isString(p)) {
+        throw new TypeError('Arguments to path.resolve must be strings');
+      } else if (!p) {
+        continue;
+      }
+
+      resolvedPath = p + '/' + resolvedPath;
+      resolvedAbsolute = p[0] === '/';
+    }
+
+    resolvedPath = normalizeArray(resolvedPath.split('/'), !resolvedAbsolute).join('/');
+    return ((resolvedAbsolute ? '/' : '') + resolvedPath) || '.';
+  }
+
+
+  /**
+   * make the relative path
+   * @param {String} from
+   * @param {String} to
+   *
+   */
+  function relative(from, to) {
+    from = resolve(from).substr(1);
+    to = resolve(to).substr(1);
+
+    var fromParts = trimArray(from.split('/'));
+    var toParts = trimArray(to.split('/'));
+
+    var length = Math.min(fromParts.length, toParts.length);
+    var samePartsLength = length;
+
+    for (var i = 0; i < length; i += 1) {
+      if (fromParts[i] !== toParts[i]) {
+        samePartsLength = i;
+        break;
+      }
+    }
+
+    var outputParts = [];
+    for (var i = samePartsLength; i < fromParts.length; i += 1) {
+      outputParts.push('..');
+    }
+
+    outputParts = outputParts.concat(toParts.slice(samePartsLength));
+
+    return outputParts.join('/');
+  }
+
+
+  /**
+   * Is it equal to [enviroment path] and [assingment path]
+   * - If the [arguments path] is relative path, and then
+   *   converted to an absolute path.
+   * @param {String} env
+   * @param {String} asg (contains wildcards[**|*])
+   */
+  function equal(env, asg) {
+
+    if (arguments.length === 1) {
+      env = cwd();
+      asg = arguments[0];
+    }
+
+    if (!isAbsolute(asg)) {
+      asg = join(cwd(), asg);
+    }
+
+    if (!isAbsolute(env)) {
+      env = join(cwd(), env);
+    }
+
+    var envParts = trimArray(env.split('/'));
+    var asgParts = trimArray(asg.split('/'));
+    var length = Math.max(envParts.length, asgParts.length);
+    var diff = envParts.length - asgParts.length;
+    var match = true;
+
+    for (var i = 0; i < length; i += 1) {
+      if ('**' === asgParts[i]) {
+        if (diff--) asgParts.splice(i, 0, '**');
+        continue;
+      }
+      if ('*' === asgParts[i]) continue;
+      if (/^(\*\.)/.test(asgParts[i]) &&
+        (extname(envParts[i]) === extname(asgParts[i]))) {
+        continue;
+      }
+      if (envParts[i] !== asgParts[i]) {
+        match = false;
+      }
+    }
+
+    return match;
   }
 
 
@@ -244,72 +342,38 @@ var Name = 'path';
 
 
   /**
-   * resolve
-   * @param {String} [from ...]
-   * @param {String} to
+   * is absolute path ?
+   * @param {String} p <pathname>
+   * @return {Boolean}
    */
-  function resolve() {
-    var resolvedPath = '';
-    var resolvedAbsolute = false;
-    var hasDomain = false;
-
-    for (var i = arguments.length - 1; i >= -1 && !resolvedAbsolute; i -= 1) {
-      var p = (i >= 0) ? arguments[i] : !hasDomain ? cwd() : '';
-
-      if (!hasDomain) {
-        hasDomain = isDomain(p);
-      }
-
-      if (!isString(p)) {
-        throw new TypeError('Arguments to path.resolve must be strings');
-      } else if (!p) {
-        continue;
-      }
-
-      resolvedPath = p + '/' + resolvedPath;
-      resolvedAbsolute = p[0] === '/';
-    }
-
-    resolvedPath = normalizeArray(resolvedPath.split('/'), !resolvedAbsolute).join('/');
-    return ((resolvedAbsolute ? '/' : '') + resolvedPath) || '.';
+  function isAbsolute(p) {
+    return p.charAt(0) === '/';
   }
 
 
   /**
-   * make the relative path
-   * @param {String} from
-   * @param {String} to
-   *
+   * is relative path ?
+   * @param {String} p <pathname>
+   * @return {Boolean}
    */
-  function relative(from, to) {
-    from = resolve(from).substr(1);
-    to = resolve(to).substr(1);
+  function isRelative(p) {
+    return !isAbsolute(p);
+  }
 
-    var fromParts = trimArray(from.split('/'));
-    var toParts = trimArray(to.split('/'));
 
-    var length = Math.min(fromParts.length, toParts.length);
-    var samePartsLength = length;
-    for (var i = 0; i < length; i++) {
-      if (fromParts[i] !== toParts[i]) {
-        samePartsLength = i;
-        break;
-      }
-    }
-
-    var outputParts = [];
-    for (var i = samePartsLength; i < fromParts.length; i += 1) {
-      outputParts.push('..');
-    }
-
-    outputParts = outputParts.concat(toParts.slice(samePartsLength));
-
-    return outputParts.join('/');
+  /**
+   * is domain ?
+   * @param {String} p <pathname>
+   * @return {Boolean}
+   */
+  function isDomain(p) {
+    return rePro.test(p);
   }
 
 
   /**
    * get the current working directory
+   * - return value is absolute path
    * @return {String}
    */
   function cwd() {
@@ -330,12 +394,13 @@ var Name = 'path';
 
 
   /**
-   * split path
+   * Split a url into
+   * [0:protocol, 1:host, 2:port, 3:path, 4:dir, 5:base, 6:ext, 7:search, 8:hash]
    * @param {String} url
    * @return {Array}
    */
   function splitPath(url) {
-    return reURL.exec(url);
+    return reURL.exec(url).slice(1);
   }
 
 
